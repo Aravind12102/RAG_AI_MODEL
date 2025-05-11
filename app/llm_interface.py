1f4e19da692d9a7faa31bd663b1f380bfa646495
Aravind12102 <145488819+Aravind12102@users.noreply.github.com>
2025-05-11 10:22:35 +0530

    Update llm_interface.py

--- app/llm_interface.py
+++ app/llm_interface.py
@@ -1,14 +1,15 @@
 import google.generativeai as genai
+import os
 
 
-genai_client = genai.Client(api_key="AIzaSyAfO8S5sipCLNhMgt70HtpFDrpuI7nanfw")  # Or use os.environ["GOOGLE_API_KEY"]
+genai_client = os.environ["G_API_KEY"]
 
 def query_llm(context, question):
     context_str = "\n".join([doc.page_content for doc in context])
     prompt = f"Use the following context to answer the question:\n\n{context_str}\n\nQuestion: {question}"
     
     response = genai_client.models.generate_content(
-        model="gemini-2.0-flash",  # You can also use "gemini-1.5-pro" if supported
+        model="gemini-2.0-flash",  
         contents=prompt
     )
     return response.text
